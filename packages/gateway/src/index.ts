b34eb54c0fdb3e7c50a548ca404b720d4b76af9c
takashimamorino <does1026@gmail.com>
2022-04-03 11:52:43 +0900

    feat: supergraph

---
+++ packages/gateway/src/index.ts
@@ -0,0 +1,17 @@
+import { ApolloServer } from "apollo-server";
+import { ApolloGateway } from "@apollo/gateway";
+import { readFileSync } from "fs";
+
+const supergraphSdl = readFileSync("./supergraph.graphql").toString();
+
+const gateway = new ApolloGateway({
+	supergraphSdl,
+});
+
+const server = new ApolloServer({
+	gateway,
+});
+
+server.listen().then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
+});
